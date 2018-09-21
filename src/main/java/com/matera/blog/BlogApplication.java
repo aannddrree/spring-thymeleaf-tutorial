68d4511608f4df77e7ee220710e53fc95577e28b
aannddrree <aannddrree.silva@gmail.com>
2018-09-21 09:22:55 -0300

    Implantação

---
+++ src/main/java/com/matera/blog/BlogApplication.java
@@ -0,0 +1,15 @@
+package com.matera.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class BlogApplication {
+	
+	//Inicializa a aplicação
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+}
